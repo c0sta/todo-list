1e5298b47618130fa8cf53eb103d0a9ee0f70328
Gabriel CM <gabriel.moura@actdigital.com>
2022-11-15 16:09:33 -0300

    chore: boilerplate files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+
 import App from './App'
+import { theme } from './global'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
