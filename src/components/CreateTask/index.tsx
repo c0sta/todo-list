2ba0a5ab7b1a14b29617f9f98d84a9db5beb1af7
Gabriel CM <gabriel.moura@actdigital.com>
2022-11-20 14:38:42 -0300

    feat: create task event

--- src/components/CreateTask/index.tsx
+++ src/components/CreateTask/index.tsx
@@ -1,10 +1,31 @@
-import { Button, Container, Input } from './styles'
+import { useState } from 'react'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
+
+import { Button, Container, Input } from './styles'
+import { useTodo } from '../../hooks'
+
 export const CreateTask = () => {
+  const [taskText, setTaskText] = useState<string>('')
+  const { actions } = useTodo()
+
+  const handleCreate = () => {
+    if (!taskText.length) return
+
+    actions.create({
+      text: taskText,
+      checked: false
+    })
+    setTaskText('')
+  }
+
   return (
     <Container>
-      <Input placeholder='Adicione uma nova tarefa' />
-      <Button>
+      <Input
+        value={taskText}
+        placeholder='Adicione uma nova tarefa'
+        onChange={e => setTaskText(e.target.value)}
+      />
+      <Button onClick={handleCreate}>
         Criar <AiOutlinePlusCircle />
       </Button>
     </Container>
